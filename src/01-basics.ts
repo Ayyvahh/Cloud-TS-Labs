ba2a7fd30304a547a8dad5a4bc7431042e440b18
ax01f x <ayyvah@hotmail.com>
2025-09-25 22:52:31 +0100

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -6,51 +6,60 @@ interface Friend {
     age: number
 }
 
-const friend1 = {
-  name: "SpongeBob Squarepants",
-  phone: "087-12345",
-  age: 25,
+interface Colleague {
+    name: "Sandy Cheeks",
+    department: "Engineering",
+    contact: {
+        email: string,
+        extension: number
+    },
+}
+
+const friend1: Friend = {
+    name: "SpongeBob Squarepants",
+    phone: "087-12345",
+    age: 25,
 };
 
-const friend2 = {
-  name: "Patrick Star",
-  phone: "086--12345",
-  age: 31,
+const friend2: Friend = {
+    name: "Patrick Star",
+    phone: "086--12345",
+    age: 31,
 };
 
-const friends = [friend1, friend2];
+const friends: Friend[] = [friend1, friend2];
 console.log(friends[1]);
 
 //   -------------------
 const colleague1 = {
-  name: "Sandy Cheeks",
-  department: "Engineering",
-  contact: {
-    email: "sandycheeks@bikinibottom.com",
-    extension: 121,
-  },
+    name: "Sandy Cheeks",
+    department: "Engineering",
+    contact: {
+        email: "sandycheeks@bikinibottom.com",
+        extension: 121,
+    },
 };
 
 const colleague2 = {
-  name: "Mr Krabs",
-  department: "Finance",
-  contact: {
-    email: "krabbs@bikinibottom.com",
-    extension: 132,
-  },
+    name: "Mr Krabs",
+    department: "Finance",
+    contact: {
+        email: "krabbs@bikinibottom.com",
+        extension: 132,
+    },
 };
 
 const colleague3 = {
-  name: "Plankton Sheldon",
-  department: "HR",
-  contact: {
-    email: "plank@chumbucket.com",
-    extension: 125,
-  },
+    name: "Plankton Sheldon",
+    department: "HR",
+    contact: {
+        email: "plank@chumbucket.com",
+        extension: 125,
+    },
 };
 const colleagues = {
-  current: [colleague1, colleague2, colleague3],
-  former: [],
+    current: [colleague1, colleague2, colleague3],
+    former: [],
 };
 
 console.log(colleagues.current[0]);
