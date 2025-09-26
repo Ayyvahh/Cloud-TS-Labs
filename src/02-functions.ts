8903a9348802a7b9780f163f4a9c06275e3e763e
ax01f x <ayyvah@hotmail.com>
2025-09-26 02:02:18 +0100

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -47,17 +47,20 @@ addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
 function sortColleagues(
   colleagues: Colleague[],
   sorter: (c1: Colleague, c2: Colleague) => number,
-  max : number
+  max? : number
 ): EmailContact[] {
-  const end = max < 2 ? 1 : max
+  let end = colleagues.length;
+  if (max !== undefined) {
+     end = max < 2 ? 1 : max
+  }
   const sorted = colleagues.sort(sorter);
   const fullResult =  sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
   return fullResult.slice(0,end)
 }
 // Test invocations
-console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension),3));
-console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
-
+//console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension),3));
+//console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
+//console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length))); // NEW
 
 
 //find friends by criterion
@@ -67,5 +70,20 @@ function findFriends(friends: Friend[], criterion: (friend: Friend) => boolean) 
     return found.map(friend => friend.name);
 }
 
-console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
-console.log(findFriends(friends, (friend) => friend.age < 35));
+//console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
+//console.log(findFriends(friends, (friend) => friend.age < 35));
+
+
+// add interests to friend
+function addInterest(friend: Friend, interest: string): string[] {
+  
+    if (!friend.interests) {
+    friend.interests = [];
+  }
+  friend.interests.push(interest);
+
+  return friend.interests;
+}
+
+
+console.log(addInterest(friends[1], 'Politics'))
